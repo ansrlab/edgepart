8181aa12740510912e4d6c697a77d52282afd7dc
Qin Liu <lqgy2001@gmail.com>
2016-09-07 20:24:37 +0800

    update

--- src/graph.cpp
+++ src/graph.cpp
@@ -2,15 +2,15 @@
 
 void graph_t::build(const std::vector<edge_t> &edges)
 {
-    std::vector<size_t> count(num_vertices, 0);
-    for (size_t i = 0; i < edges.size(); i++)
-        count[edges[i].first]++;
-    if (neighbors == NULL)
-        neighbors = (size_t *)malloc(sizeof(size_t) * edges.size());
-    else if (edges.size() > nedges)
+    if (edges.size() > nedges)
         neighbors = (size_t *)realloc(neighbors, sizeof(size_t) * edges.size());
     CHECK(neighbors) << "allocation failed";
     nedges = edges.size();
+
+    std::vector<size_t> count(num_vertices, 0);
+    for (size_t i = 0; i < nedges; i++)
+        count[edges[i].first]++;
+
     vdata[0] = adjlist_t(neighbors);
     for (vid_t v = 1; v < num_vertices; v++) {
         count[v] += count[v-1];
@@ -22,15 +22,15 @@ void graph_t::build(const std::vector<edge_t> &edges)
 
 void graph_t::build_reverse(const std::vector<edge_t> &edges)
 {
-    std::vector<size_t> count(num_vertices, 0);
-    for (size_t i = 0; i < edges.size(); i++)
-        count[edges[i].second]++;
-    if (neighbors == NULL)
-        neighbors = (size_t *)malloc(sizeof(size_t) * edges.size());
-    else if (edges.size() > nedges)
+    if (edges.size() > nedges)
         neighbors = (size_t *)realloc(neighbors, sizeof(size_t) * edges.size());
     CHECK(neighbors) << "allocation failed";
     nedges = edges.size();
+
+    std::vector<size_t> count(num_vertices, 0);
+    for (size_t i = 0; i < nedges; i++)
+        count[edges[i].second]++;
+
     vdata[0] = adjlist_t(neighbors);
     for (vid_t v = 1; v < num_vertices; v++) {
         count[v] += count[v-1];
